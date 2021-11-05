f092b940b1cf6e9540d6cd908fb27fa9ef3e8f16
Uchihaki <93758746+Uchihaki@users.noreply.github.com>
2021-11-05 23:16:29 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,12 +7,12 @@ const CONFIG = {
     title: 'Phải chăng anh đã yêu ngay từ lần đầu nhìn thấy cái bánh - và em ^^ 🥰',
     desc: 'Làm người yêu anh nhé <3333',
     btnYes: 'Oke yêu luôn',
-    btnNo: 'Không nha :3 ( ngon bấm vào đây - Khang said)',
+    btnNo: 'Không nha :3',
     question: 'Trai Đức Trọng không thiếu, mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho người eo',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'Em hay đấy 🥰. Yêu em nhiều 😘😘',
     messDesc: 'Hẹn em một ngày không xa, ngày nào thì anh chưa biết :)) btw cảm ơn Trang với Mi ^^',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://www.youtube.com/watch?v=EBHOXV1ihus' 
+    messLink: 'https://www.youtube.com/watch?v=0Xl02jNm4WQ' 
 }
