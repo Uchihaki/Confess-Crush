eaa94a76c3aa83b8c56b989b1790a4950aebcc19
Uchihaki <93758746+Uchihaki@users.noreply.github.com>
2021-11-05 14:48:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,13 +1,13 @@
 const CONFIG = {
     titleWeb: "loitotinhmuonmang",
     introTitle: 'Phạm Thụy Kim Ngọc',
-    introDesc: ` Ngoài kia mây trắng nắng vàng
+    introDesc: ` Ngoài kia mây trắng nắng vàng -      
     Lòng anh chỉ có một người là em `,
     btnIntro: ' Bấm vào đây nè Còi ^^',
-    title: 'Phải chăng anh đã yêu ngay từ lần đầu nhìn thấy cái bánh (và em ^^) 🥰',
+    title: 'Phải chăng anh đã yêu ngay từ lần đầu nhìn thấy cái bánh - và em ^^ 🥰',
     desc: 'Làm người yêu anh nhé <3333',
     btnYes: 'Oke yêu luôn',
-    btnNo: 'Không nha :3',
+    btnNo: 'Không nha :3 ( ngon bấm vào đây - Khang said)',
     question: 'Trai Đức Trọng không thiếu, mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho người eo',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
